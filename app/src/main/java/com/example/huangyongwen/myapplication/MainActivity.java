131e23ee0cde26d0c330bf9f8bf0a3d54441e10d
huangyongwen <huangyongwen@meizu.com>
2020-05-08 18:05:53 +0800

    1. 初次提交

---
+++ app/src/main/java/com/example/huangyongwen/myapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.huangyongwen.myapplication;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.sackcentury.shinebuttonlib.ShineButton;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
