be9714b3b58eabd486cb228148d6413fbc7f7132
huangyongwen <huangyongwen0306@163.com>
2020-05-09 00:29:58 +0800

    1. 测试APP

--- app/src/main/java/com/example/huangyongwen/myapplication/MainActivity.java
+++ app/src/main/java/com/example/huangyongwen/myapplication/MainActivity.java
@@ -1,11 +1,7 @@
 package com.example.huangyongwen.myapplication;
 
 import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.widget.LinearLayout;
-
-import com.sackcentury.shinebuttonlib.ShineButton;
 
 public class MainActivity extends Activity {
 
